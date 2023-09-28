7f8d6947be8f871e8c58704e4391f5c37371614d
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-28 18:28:15 +0200

    form module

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -6,10 +6,18 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
+import { ComponentsComponent } from './components/components.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [
+    ComponentsComponent
+  ],
+  imports: [CommonModule, MatInputModule, MatButtonModule, MatSelectModule, MatRadioModule, MatCardModule, ReactiveFormsModule],
   exports: [
     MatDatepickerModule,
     MatFormFieldModule,
