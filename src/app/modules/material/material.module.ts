7d59cc1444b98ec99ec7a3d9b7ea8cca1b421bbb
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-28 17:57:25 +0200

    buttons module

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -5,10 +5,16 @@ import { CommonModule } from '@angular/common';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatDatepickerModule, MatFormFieldModule, MatNativeDateModule],
+  exports: [
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatButtonModule,
+  ],
 })
 export class MaterialModule {}
